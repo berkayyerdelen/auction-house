1bdddbd90071988c936ee554ce75cd73af81b2c1
berkayyerdelen <b.yerdelen@parkbee.com>
2025-08-26 21:03:52 +0200

    built error

--- src/main/java/com/acme/auctionhouse/AuctionHouseApplication.java
+++ src/main/java/com/acme/auctionhouse/AuctionHouseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ScheduledProperties.class)
+
 public class AuctionHouseApplication {
 
 	public static void main(String[] args) {
